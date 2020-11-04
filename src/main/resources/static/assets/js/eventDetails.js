add5dfdc1f2fa672dac50adf4d83bc1c01afe796
Alf <62615304+alf-melmac@users.noreply.github.com>
2020-11-04 23:18:49 +0100

    Finished addEventToChannel button

---
+++ src/main/resources/static/assets/js/eventDetails.js
@@ -0,0 +1,11 @@
+$(function () {
+    "use strict";
+
+    $('.copy-to-clipboard input').on('click', function () {
+        const $this = $(this);
+        $this.focus();
+        $this.select();
+        document.execCommand('copy');
+        $(".copied").text("Copied to clipboard").show().fadeOut(1200);
+    });
+});
