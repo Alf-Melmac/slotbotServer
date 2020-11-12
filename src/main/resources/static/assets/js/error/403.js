8f28260527897aa2ba61359cd511b20861d9bd3b
Alf <62615304+alf-melmac@users.noreply.github.com>
2020-11-13 00:40:12 +0100

    Added custom 403 page

---
+++ src/main/resources/static/assets/js/error/403.js
@@ -0,0 +1,29 @@
+$(function () {
+    "use strict";
+
+    const $root = $('html');
+    const $eyef = $('#eyef');
+    let cx = $eyef.attr('cx');
+    let cy = $eyef.attr('cy');
+
+    $root.on('mousemove', function (evt) {
+        let x = evt.clientX / innerWidth;
+        let y = evt.clientY / innerHeight;
+
+        $root.prop('--mouse-x', x);
+        $root.prop('--mouse-y', y);
+
+        cx = 115 + 30 * x;
+        cy = 50 + 30 * y;
+        $eyef.attr("cx", cx);
+        $eyef.attr("cy", cy);
+    });
+
+    $root.on('touchmove', function (e) {
+        let x = e.originalEvent.touches[0].clientX / innerWidth;
+        let y = e.originalEvent.touches[0].clientY / innerHeight;
+
+        $root.prop('--mouse-x', x);
+        $root.prop('--mouse-y', y);
+    });
+});
