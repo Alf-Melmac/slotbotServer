0325efda367a332e3b154399abfd6a3ca0b906d4
Alf <62615304+alf-melmac@users.noreply.github.com>
2020-11-13 19:34:35 +0100

    Sets indeterminate prop to checkboxes to make it possible that the selection does not have to be made while editing too

--- src/main/resources/static/assets/js/eventEdit.js
+++ src/main/resources/static/assets/js/eventEdit.js
@@ -1,9 +1,18 @@
 $(function () {
     "use strict";
 
+    setIndeterminateIfRequired('respawn');
+    setIndeterminateIfRequired('reserveParticipating');
+
     $('#btnCancel').on('click', () => window.location.href = eventDetailsUrl);
 
     $('#btnFinish').on('click', function () {
         saveEvent($(this), putEventUrl, 'PUT', true);
     });
 });
+
+function setIndeterminateIfRequired(dtoKey) {
+    if (savedEvent[dtoKey] === null) {
+        $(`:checkbox[data-dtokey='${dtoKey}']`).prop('indeterminate', true);
+    }
+}
