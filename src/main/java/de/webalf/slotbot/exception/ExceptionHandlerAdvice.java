85fa8837567a0ab692d2559cd2cd03eef189dfad
Alf <62615304+Alf-Melmac@users.noreply.github.com>
2021-04-12 21:49:14 +0200

    Fixed and improved rest endpoint validation

--- src/main/java/de/webalf/slotbot/exception/ExceptionHandlerAdvice.java
+++ src/main/java/de/webalf/slotbot/exception/ExceptionHandlerAdvice.java
@@ -2,15 +2,22 @@ package de.webalf.slotbot.exception;
 
 import de.webalf.slotbot.util.StringUtils;
 import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Handles exceptions thrown in all layers.
@@ -32,6 +39,19 @@ class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler 
 						.requestedURI(request.getRequestURI())
 						.build(),
 				determineHttpStatus(ex));
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+		final List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+		final String errorMessage = fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining(", "));
+
+		return new ResponseEntity<>(
+				ExceptionResponse.builder()
+						.errorMessage(errorMessage + (fieldErrors.size() > 1 ? " are invalid." : errorMessage + " is invalid.") + " Missing mandatory field?")
+						.requestedURI(((ServletWebRequest) request).getRequest().getRequestURI())
+						.build(),
+				HttpStatus.BAD_REQUEST);
 	}
 
 	/**
