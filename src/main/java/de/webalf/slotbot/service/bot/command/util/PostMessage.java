446a9c586ae3c1a4330b38a85e2be5a4eddf7f84
Alf <62615304+Alf-Melmac@users.noreply.github.com>
2021-07-29 20:32:23 +0200

    Added postMessage slash command

--- src/main/java/de/webalf/slotbot/service/bot/command/util/PostMessage.java
+++ src/main/java/de/webalf/slotbot/service/bot/command/util/PostMessage.java
@@ -1,12 +1,19 @@
 package de.webalf.slotbot.service.bot.command.util;
 
 import de.webalf.slotbot.model.annotations.Command;
+import de.webalf.slotbot.model.annotations.SlashCommand;
 import de.webalf.slotbot.service.bot.command.DiscordCommand;
+import de.webalf.slotbot.service.bot.command.DiscordSlashCommand;
+import de.webalf.slotbot.util.bot.SlashCommandUtils;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 
 import java.util.List;
 
+import static de.webalf.slotbot.util.bot.InteractionUtils.finishedSlashCommandAction;
 import static de.webalf.slotbot.util.bot.MessageUtils.deleteMessagesInstant;
 import static de.webalf.slotbot.util.permissions.BotPermissionHelper.Authorization.EVENT_MANAGE;
 
@@ -20,7 +27,11 @@ import static de.webalf.slotbot.util.permissions.BotPermissionHelper.Authorizati
 		usage = "<Nachricht>",
 		argCount = {1},
 		authorization = EVENT_MANAGE)
-public class PostMessage implements DiscordCommand {
+@SlashCommand(name = "postMessage",
+		description = "Lässt den Bot die übergebene Nachricht in den gleichen Kanal versenden.",
+		authorization = EVENT_MANAGE,
+		optionPosition = 0)
+public class PostMessage implements DiscordCommand, DiscordSlashCommand {
 	@Override
 	public void execute(Message message, List<String> args) {
 		log.trace("Command: postMessage");
@@ -28,5 +39,27 @@ public class PostMessage implements DiscordCommand {
 		message.getChannel().sendMessage(args.get(0)).queue();
 
 		deleteMessagesInstant(message);
+	}
+
+	private static final String OPTION_MESSAGE = "nachricht";
+	private static final List<List<OptionData>> OPTIONS = List.of(
+			List.of(new OptionData(OptionType.STRING, OPTION_MESSAGE, "Zu versendender Text.", true))
+	);
+
+	@Override
+	public void execute(SlashCommandEvent event) {
+		log.trace("Slash command: postMessage");
+
+		@SuppressWarnings("ConstantConditions") //Required option
+		final String message = SlashCommandUtils.getStringOption(event.getOption(OPTION_MESSAGE));
+
+		event.getChannel().sendMessage(message).queue();
+
+		finishedSlashCommandAction(event);
+	}
+
+	@Override
+	public List<OptionData> getOptions(int optionPosition) {
+		return OPTIONS.get(optionPosition);
 	}
 }
