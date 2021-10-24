cb60303a9f12c39e2598dcd9e55a8d79b892d290
Alf <62615304+Alf-Melmac@users.noreply.github.com>
2021-10-24 22:45:12 +0200

    Added new daa spacer Improved DAA text colors

--- src/main/java/de/webalf/slotbot/service/bot/command/event/AddEventToChannel.java
+++ src/main/java/de/webalf/slotbot/service/bot/command/event/AddEventToChannel.java
@@ -184,7 +184,7 @@ public class AddEventToChannel implements DiscordCommand, DiscordSlashCommand, D
 			final long ownerGuild = Long.parseLong(eventApiDto.getOwnerGuild());
 			String spacer;
 			if (isDAA(ownerGuild)) {
-				spacer = "https://cdn.discordapp.com/attachments/759147249325572097/899736523275124827/Discord_Missionstrenner_DAA.png";
+				spacer = "https://cdn.discordapp.com/attachments/759147249325572097/901925677677297724/Trenner.png";
 			} else {
 				spacer = "https://cdn.discordapp.com/attachments/759147249325572097/798539020677808178/Discord_Missionstrenner.png";
 			}
