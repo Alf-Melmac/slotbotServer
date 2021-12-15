d74ce495ef4faaeaec6107468fc24e292fbd5e8f
Alf <62615304+Alf-Melmac@users.noreply.github.com>
2021-12-15 22:15:48 +0100

    Added robots.txt

--- src/main/java/de/webalf/slotbot/controller/website/FileWebController.java
+++ src/main/java/de/webalf/slotbot/controller/website/FileWebController.java
@@ -3,7 +3,9 @@ package de.webalf.slotbot.controller.website;
 import de.webalf.slotbot.service.FileService;
 import de.webalf.slotbot.util.GuildUtils.Guild;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -11,10 +13,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 import static de.webalf.slotbot.util.EventCalendarUtil.ICS_FILE_EXTENSION;
+import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
 
 /**
  * @author Alf
@@ -22,6 +29,7 @@ import static de.webalf.slotbot.util.EventCalendarUtil.ICS_FILE_EXTENSION;
  */
 @Controller
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
+@Slf4j
 public class FileWebController {
 	private final FileService fileService;
 
@@ -52,5 +60,17 @@ public class FileWebController {
 				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=\"" + file.getFilename() + "\"")
 				.contentType(MediaType.APPLICATION_OCTET_STREAM)
 				.body(file);
+	}
+
+	@GetMapping(value = "/robots.txt", produces = TEXT_PLAIN_VALUE)
+	@ResponseBody
+	public String getRobotsTxt() {
+		try {
+			File robotsTxt = new ClassPathResource("static/assets/robots.txt").getFile();
+			return new String(Files.readAllBytes(robotsTxt.toPath()));
+		} catch (IOException e) {
+			log.error("Failed to get robots.txt", e);
+			return "";
+		}
 	}
 }
