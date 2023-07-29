60b68d7100c85c7c9ba53ea47943d52f3cc7918d
Alf <62615304+Alf-Melmac@users.noreply.github.com>
2023-07-29 23:23:52 +0200

    Add force unslot user slash command

--- src/main/java/de/webalf/slotbot/service/bot/command/event/Unslot.java
+++ src/main/java/de/webalf/slotbot/service/bot/command/event/Unslot.java
@@ -16,6 +16,7 @@ import java.util.List;
 
 import static de.webalf.slotbot.util.bot.InteractionUtils.finishedVisibleInteraction;
 import static de.webalf.slotbot.util.bot.SlashCommandUtils.getOptionalIntegerOption;
+import static de.webalf.slotbot.util.bot.SlashCommandUtils.getOptionalUserOption;
 
 /**
  * @author Alf
@@ -30,12 +31,18 @@ import static de.webalf.slotbot.util.bot.SlashCommandUtils.getOptionalIntegerOpt
 		description = "bot.slash.event.unslot.force.description",
 		authorization = Permission.MANAGE_CHANNEL,
 		optionPosition = 0)
+@SlashCommand(name = "bot.slash.event.unslot.force.user",
+		description = "bot.slash.event.unslot.force.description",
+		authorization = Permission.MANAGE_CHANNEL,
+		optionPosition = 1)
 public class Unslot implements DiscordSlashCommand {
 	private final EventBotService eventBotService;
 
 	private static final String OPTION_SLOT_NUMBER = "bot.slash.event.unslot.option.number";
+	private static final String OPTION_USER = "bot.slash.event.unslot.option.user";
 	private static final List<List<TranslatableOptionData>> OPTIONS = List.of(
-			List.of(new TranslatableOptionData(OptionType.INTEGER, OPTION_SLOT_NUMBER, "bot.slash.event.unslot.option.number.description", true))
+			List.of(new TranslatableOptionData(OptionType.INTEGER, OPTION_SLOT_NUMBER, "bot.slash.event.unslot.option.number.description", true)),
+			List.of(new TranslatableOptionData(OptionType.USER, OPTION_USER, "bot.slash.event.unslot.option.user.description", true))
 	);
 
 	@Override
@@ -43,10 +50,13 @@ public class Unslot implements DiscordSlashCommand {
 		log.trace("Slash command: unslot");
 
 		final Integer slotNumber = getOptionalIntegerOption(event, OPTION_SLOT_NUMBER);
-		if (slotNumber == null) { //Self unslot
-			eventBotService.unslot(event.getChannel().getIdLong(), event.getUser().getId());
-		} else { //Unslot others
+		final Long user = getOptionalUserOption(event, OPTION_USER);
+		if (slotNumber != null) { //Unslot by slot number
 			eventBotService.unslot(event.getChannel().getIdLong(), slotNumber);
+		} else if (user != null) { //Unslot by user
+			eventBotService.unslot(event.getChannel().getIdLong(), Long.toString(user));
+		} else { //Self unslot
+			eventBotService.unslot(event.getChannel().getIdLong(), event.getUser().getId());
 		}
 
 		finishedVisibleInteraction(event);
