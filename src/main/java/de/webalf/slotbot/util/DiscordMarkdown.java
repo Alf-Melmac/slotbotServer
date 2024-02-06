a1090095c1151427f6d5c4ab8a2fff51398805f6
TheConen <theconen@gmail.com>
2024-02-06 17:40:17 +0100

    feat: Add support for escaped markdown characters.
    If a character is escaped in markdown with a backslash, that character will be ignored when converting formatting to HTML. The backslash gets removed after all Markdown formatting options have been processed so that only the escaped character remains in the converted HTML.

--- src/main/java/de/webalf/slotbot/util/DiscordMarkdown.java
+++ src/main/java/de/webalf/slotbot/util/DiscordMarkdown.java
@@ -22,6 +22,7 @@ import java.util.regex.Pattern;
  */
 @UtilityClass
 public final class DiscordMarkdown {
+
     private static final Map<Pattern, HTML_TAG> MARKDOWN_TO_HTML_TAG_MAPPINGS = new LinkedHashMap<>();
     private static final Safelist SAFELIST = Safelist.none();
 
@@ -29,16 +30,17 @@ public final class DiscordMarkdown {
         // The order matters (hence a LinkedHashMap).
         // Markdown formatting that consists of multiple characters (eg. __underline__) needs to be processed
         // before formatting that consists of lesser characters (eg. _italic_).
-        MARKDOWN_TO_HTML_TAG_MAPPINGS.put(Pattern.compile("(?m)^#{3}\\s(?<content>.+?)(\n|$)"), HTML_TAG.HTML_HEADING_3);
-        MARKDOWN_TO_HTML_TAG_MAPPINGS.put(Pattern.compile("(?m)^#{2}\\s(?<content>.+?)(\n|$)"), HTML_TAG.HTML_HEADING_2);
-        MARKDOWN_TO_HTML_TAG_MAPPINGS.put(Pattern.compile("(?m)^#\\s(?<content>.+?)(\n|$)"), HTML_TAG.HTML_HEADING_1);
+        MARKDOWN_TO_HTML_TAG_MAPPINGS.put(Pattern.compile("(?m)^(?<!\\\\)#{3}\\s(?<content>.+?)(\n|$)"), HTML_TAG.HTML_HEADING_3);
+        MARKDOWN_TO_HTML_TAG_MAPPINGS.put(Pattern.compile("(?m)^(?<!\\\\)#{2}\\s(?<content>.+?)(\n|$)"), HTML_TAG.HTML_HEADING_2);
+        MARKDOWN_TO_HTML_TAG_MAPPINGS.put(Pattern.compile("(?m)^(?<!\\\\)#\\s(?<content>.+?)(\n|$)"), HTML_TAG.HTML_HEADING_1);
         MARKDOWN_TO_HTML_TAG_MAPPINGS.put(Pattern.compile("\n"), HTML_TAG.HTML_BREAK);
-        MARKDOWN_TO_HTML_TAG_MAPPINGS.put(Pattern.compile("\\*{3}(?<content>.+?)\\*{3}"), HTML_TAG.HTML_STRONG_ITALIC);
-        MARKDOWN_TO_HTML_TAG_MAPPINGS.put(Pattern.compile("\\*{2}(?<content>.+?)\\*{2}"), HTML_TAG.HTML_STRONG);
-        MARKDOWN_TO_HTML_TAG_MAPPINGS.put(Pattern.compile("_{2}(?<content>.+?)_{2}"), HTML_TAG.HTML_UNDERLINE);
-        MARKDOWN_TO_HTML_TAG_MAPPINGS.put(Pattern.compile("~{2}(?<content>.+?)~{2}"), HTML_TAG.HTML_STRIKETHROUGH);
-        MARKDOWN_TO_HTML_TAG_MAPPINGS.put(Pattern.compile("\\*(?<content>.+?)\\*"), HTML_TAG.HTML_ITALIC);
-        MARKDOWN_TO_HTML_TAG_MAPPINGS.put(Pattern.compile("_(?<content>.+?)_"), HTML_TAG.HTML_ITALIC);
+        MARKDOWN_TO_HTML_TAG_MAPPINGS.put(Pattern.compile("(?<!\\\\)\\*{3}(?<content>.+?)\\*{3}"), HTML_TAG.HTML_STRONG_ITALIC);
+        MARKDOWN_TO_HTML_TAG_MAPPINGS.put(Pattern.compile("(?<!\\\\)\\*{2}(?<content>.+?)\\*{2}"), HTML_TAG.HTML_STRONG);
+        MARKDOWN_TO_HTML_TAG_MAPPINGS.put(Pattern.compile("(?<!\\\\)_{2}(?<content>.+?)_{2}"), HTML_TAG.HTML_UNDERLINE);
+        MARKDOWN_TO_HTML_TAG_MAPPINGS.put(Pattern.compile("(?<!\\\\)~{2}(?<content>.+?)~{2}"), HTML_TAG.HTML_STRIKETHROUGH);
+        MARKDOWN_TO_HTML_TAG_MAPPINGS.put(Pattern.compile("(?<!\\\\)\\*(?<content>.+?)\\*"), HTML_TAG.HTML_ITALIC);
+        MARKDOWN_TO_HTML_TAG_MAPPINGS.put(Pattern.compile("(?<!\\\\)_(?<content>.+?)_"), HTML_TAG.HTML_ITALIC);
+        MARKDOWN_TO_HTML_TAG_MAPPINGS.put(Pattern.compile("\\\\(?<content>[*_`~\\\\])"), HTML_TAG.NOTHING);
 
         SAFELIST.addTags("br", "s", "u", "strong", "em", "h1", "h2", "h3");
     }
@@ -58,14 +60,18 @@ public final class DiscordMarkdown {
         MARKDOWN_TO_HTML_TAG_MAPPINGS.forEach((pattern, htmlTag) -> {
             Matcher matcher = pattern.matcher(result.get());
             result.set(matcher.replaceAll(matchResult -> {
-                        String content = "";
+                        Optional<String> content = Optional.empty();
                         try {
-                            content = matchResult.group("content");
+                            content = Optional.ofNullable(matchResult.group("content"));
+                            // Funnily enough, if the escape character (backslash) is matched, we need to escape it again, otherwise replacing it will not work
+                            if (content.isPresent() && "\\".equals(content.get())) {
+                                content = Optional.of("\\\\");
+                            }
                         } catch (IllegalArgumentException e) {
                             // Do nothing, this is fine, group "content" does not exist
                         }
                         return htmlTag.getOpeningTag() +
-                                content +
+                                content.orElse("") +
                                 htmlTag.getClosingTag()
                                         .orElse("");
                     }
@@ -84,7 +90,8 @@ public final class DiscordMarkdown {
         HTML_STRONG_ITALIC("<strong><em>", "</strong></em>"),
         HTML_HEADING_1("<h1>", "</h1>"),
         HTML_HEADING_2("<h2>", "</h2>"),
-        HTML_HEADING_3("<h3>", "</h3>");
+        HTML_HEADING_3("<h3>", "</h3>"),
+        NOTHING("", null);
 
         @Getter
         private final String openingTag;
