a9c3725cc5097cda4e01a5736b664def01652c06
Alf <62615304+Alf-Melmac@users.noreply.github.com>
2024-08-02 00:25:31 +0200

    Remove unnecessary user agent in discord oauth requests Let Spring Security use Spring MVC CORS Config and apply it only when needed, in dev mode

--- src/main/java/de/webalf/slotbot/configuration/WebMvcConfig.java
+++ src/main/java/de/webalf/slotbot/configuration/WebMvcConfig.java
@@ -3,17 +3,21 @@ package de.webalf.slotbot.configuration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 /**
+ * Allow other origins than localhost to test different guilds locally
+ *
  * @author Alf
  * @since 09.01.2022
  */
 @Configuration
 @RequiredArgsConstructor
+@Profile("dev")
 public class WebMvcConfig implements WebMvcConfigurer {
 	@Value("${server.cors.allowed-origins}")
 	private String[] allowedOrigins;
--- src/main/java/de/webalf/slotbot/configuration/authentication/website/OAuth2EndpointConfig.java
+++ src/main/java/de/webalf/slotbot/configuration/authentication/website/OAuth2EndpointConfig.java
@@ -6,18 +6,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.servlet.server.CookieSameSiteSupplier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.RequestEntity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.session.SessionRegistry;
-import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
-import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
-import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
-import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequestEntityConverter;
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequestEntityConverter;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
@@ -28,10 +20,7 @@ import org.springframework.security.web.csrf.XorCsrfTokenRequestAttributeHandler
 import org.springframework.security.web.header.writers.ClearSiteDataHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import java.util.Objects;
-
 import static org.springframework.boot.web.server.Cookie.SameSite.STRICT;
-import static org.springframework.security.config.Customizer.withDefaults;
 import static org.springframework.security.web.header.writers.ClearSiteDataHeaderWriter.Directive.COOKIES;
 
 /**
@@ -65,7 +54,6 @@ public class OAuth2EndpointConfig {
 		final CsrfTokenRequestHandler requestHandler = delegate::handle;
 
 		http // all non api requests handled here
-				.cors(withDefaults())
 				.csrf(csrf -> csrf
 						.csrfTokenRepository(tokenRepository)
 						.csrfTokenRequestHandler(requestHandler))
@@ -80,9 +68,6 @@ public class OAuth2EndpointConfig {
 						.loginPage("/oauth2/authorization/discord")
 						.defaultSuccessUrl("/events")
 						.successHandler(authenticationSuccessHandler)
-						.tokenEndpoint(tokenEndpoint -> tokenEndpoint
-								.accessTokenResponseClient(accessTokenResponseClient())
-						)
 						.userInfoEndpoint(userInfo -> userInfo
 								.userService(oAuthUserService())
 						)
@@ -101,40 +86,7 @@ public class OAuth2EndpointConfig {
 	}
 
 	@Bean
-	OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> accessTokenResponseClient() {
-		DefaultAuthorizationCodeTokenResponseClient client = new DefaultAuthorizationCodeTokenResponseClient();
-
-		client.setRequestEntityConverter(new OAuth2AuthorizationCodeGrantRequestEntityConverter() {
-			@Override
-			public RequestEntity<?> convert(OAuth2AuthorizationCodeGrantRequest oauth2Request) {
-				return withUserAgent(Objects.requireNonNull(super.convert(oauth2Request)));
-			}
-		});
-
-		return client;
-	}
-
-	@Bean
 	OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuthUserService() {
-		DefaultOAuth2UserService service = new CustomOAuth2UserService(guildUsersService, userService);
-
-		service.setRequestEntityConverter(new OAuth2UserRequestEntityConverter() {
-			@Override
-			public RequestEntity<?> convert(OAuth2UserRequest userRequest) {
-				return withUserAgent(Objects.requireNonNull(super.convert(userRequest)));
-			}
-		});
-
-		return service;
-	}
-
-	private static final String DISCORD_BOT_USER_AGENT = "Discord-OAuth";
-
-	private static RequestEntity<?> withUserAgent(RequestEntity<?> request) {
-		HttpHeaders headers = new HttpHeaders();
-		headers.putAll(request.getHeaders());
-		headers.add(HttpHeaders.USER_AGENT, DISCORD_BOT_USER_AGENT);
-
-		return new RequestEntity<>(request.getBody(), headers, request.getMethod(), request.getUrl());
+		return new CustomOAuth2UserService(guildUsersService, userService);
 	}
 }
